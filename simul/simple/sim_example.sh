2a5049c4bca5f2b822646cd4bff05cd288acd797
Mike Schatz <mschatz@cs.jhu.edu>
2017-08-24 10:29:52 -0400

    add

--- simul/simple/sim_example.sh
+++ simul/simple/sim_example.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-## Make sure HAPCUT, NGMLR, SURVIVOR, SNIFFLES, SAMTOOLS are in your path
+## Make sure HAPCUT, NGMLR, SURVIVOR-LRSIM, SNIFFLES, SAMTOOLS are in your path
 
+SNP_DIST=100
+BASE=base.fa
+SURVIVOR-LRSIM 0 base.fa simul.param 0 mut $SNPDIST
