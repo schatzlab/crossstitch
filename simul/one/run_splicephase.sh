287d57f5c90f45b8952c5d20881587dd43cef657
Michael Schatz <mschatz@github.com>
2017-08-25 17:08:51 -0400

    add

---
+++ simul/one/run_splicephase.sh
@@ -0,0 +1,1 @@
+../../splicephase.pl data/pbAll.phased.vcf data/pbAll.sniffles.vcf data/pbAll.hairs data/spliced.out
--- splicephase.pl
+++ splicephase.pl
@@ -14,6 +14,13 @@ open SNIFFLESVCF, $SNIFFLESVCFFILE or die "Cant open $SNIFFLESVCFFILE ($!)\n";
 open READHAIRS,   $READHAIRSFILE   or die "Cant open $READHAIRSFILE ($!)\n";
 open OUTVCF,       "> $OUTVCFFILE" or die "Cant open $OUTVCFFILE ($!)\n";
 
+
+
+
+
+## Load the phased VCF file
+###############################################################################
+
 my @vcfheader;
 my $vcfdata;
 
@@ -22,6 +29,7 @@ my $vcfdatalines = 0;
 
 while (<PHASEDVCF>)
 {
+  chomp;
   if (/^#/) 
   {
     push @vcfheader, $_;
@@ -49,6 +57,70 @@ while (<PHASEDVCF>)
 
 print "Loaded $vcfheaderlines header lines and $vcfdatalines variants\n";
 
+
+## Load Sniffles SV calls
+###############################################################################
+
+my $readstophase;
+my $snifflesvariants;
+my $sniffleslines = 0;
+while (<SNIFFLESVCF>)
+{
+  if (/^#/) 
+  {
+    ## nothing to do
+  }
+  else
+  {
+    chomp;
+    my ($chrom, $pos, $id, $ref, $alt, $qual, $filter, $info, $format, $sample) = split /\s+/, $_; 
+    my $v;
+    $v->{chrom}  = $chrom;
+    $v->{pos}    = $pos;
+    $v->{id}     = $id;
+    $v->{ref}    = $ref;
+    $v->{alt}    = $alt;
+    $v->{qual}   = $qual;
+    $v->{filter} = $filter;
+    $v->{info}   = $info;
+    $v->{format} = $format;
+    $v->{sample} = $sample;
+    $v->{reads}  = [];
+
+    my @infofields = split /;/, $info;
+    foreach my $f (@infofields)
+    {
+      if ($f =~ /^RNAMES/)
+      {
+         $f = substr($f, 7);
+         my @reads = split /,/, $f;
+         foreach my $r (@reads)
+         {
+           $readstophase->{$r}->{num}++;
+           $readstophase->{$r}->{sv}->{$chrom}->{$pos}->{phase} = -1;
+           push @{$v->{reads}}, $r;
+         }
+      }
+    }
+
+    $snifflesvariants->{$chrom}->{$pos} = $v;
+    $sniffleslines++;
+  }
+}
+
+my $readcount = scalar keys %$readstophase;
+print "Loaded $sniffleslines sniffles variants involving $readcount reads\n";
+
+foreach my $r (sort keys %$readstophase)
+{
+	my $num = $readstophase->{$r}->{num};
+	print "  $r $num\n";
+}
+
+
+## Determine the read phase information
+###############################################################################
+
 my $hairslines = 0;
 while (<READHAIRS>)
 {
@@ -56,14 +128,6 @@ while (<READHAIRS>)
 }
 
 print "Loaded $hairslines hairs records\n";
-
-my $sniffleslines = 0;
-while (<SNIFFLESVCF>)
-{
-  $sniffleslines++;
-}
-
-print "Loaded $sniffleslines sniffles variants\n";
 
 
 print OUTVCF "not done";
