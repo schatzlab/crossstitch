ba307e31b36796a5a05b281ccf469fba1db4da91
Michael Schatz <mschatz@github.com>
2017-09-12 09:04:45 -0400

    update

--- simul/simple/sim_example.sh
+++ simul/simple/sim_example.sh
@@ -13,12 +13,20 @@ THREADS=10
 
 mkdir -p data
 
+if [ ! -r base.fa.amb ]
+then 
+  echo "Indexing genome for bwa"
+  bwa index base.fa
+fi
 
 if [ ! -r data/mutA.fasta ]
 then
   echo "Simulating variants"
   SURVIVOR-LRSIM 0 $BASE $PARAM 0 data/mut $SNPDIST
 fi
+
+## Simulate PacBio Reads
+###############################################################################
 
 if [ ! -r data/pbA.fa ]
 then
@@ -34,7 +42,7 @@ fi
 
 if [ ! -r data/pbAll.fa ]
 then
-  echo "reformating reads"
+  echo "reformating pacbio reads"
   awk '{if (/^>/){c++; print ">pbread"c;}else{print}}' data/pbA.fa data/pbB.fa > data/pbAll.fa
 fi
 
@@ -48,19 +56,22 @@ then
   rm -f data/pbAll.unsorted.bam
 fi
 
-
 if [ ! -r data/pbAll.sniffles.vcf ]
 then
   echo "Calling variants with Sniffles"
   READSTOPHASE=1000
-  /work-zfs/mschatz1/mschatz/build/Sniffles/bin/sniffles-core-1.0.6/sniffles -m data/pbAll.bam -v data/pbAll.sniffles.vcf --cluster --genotype -n $READSTOPHASE
+  /work-zfs/mschatz1/mschatz/build/Sniffles/bin/sniffles-core-1.0.6/sniffles -m data/pbAll.bam -v data/pbAll.sniffles.vcf --cluster --genotype --report_seq -n $READSTOPHASE
 fi
+
+
+## Simulate Illumina Reads
+###############################################################################
 
 
 if [ ! -r data/illA.1.fq ]
 then
   samtools faidx data/mutA.fasta
-  numpairs=`awk '{print $2*30/200}' data/mutA.fasta.fai`
+  numpairs=`awk '{print $2*50/200}' data/mutA.fasta.fai`
   echo "simulating $numpairs pairs for hapB"
   mason_simulator -ir data/mutA.fasta -n $numpairs -o data/illA.1.fq -or data/illA.2.fq --num-threads $THREADS
 fi
@@ -68,7 +79,7 @@ fi
 if [ ! -r data/illB.1.fq ]
 then
   samtools faidx data/mutB.fasta
-  numpairs=`awk '{print $2*30/200}' data/mutB.fasta.fai`
+  numpairs=`awk '{print $2*50/200}' data/mutB.fasta.fai`
   echo "simulating $numpairs pairs for hapB"
   mason_simulator -ir data/mutB.fasta -n $numpairs -o data/illB.1.fq -or data/illB.2.fq --num-threads $THREADS
 fi
@@ -80,11 +91,6 @@ then
  cat data/illA.2.fq data/illB.2.fq | paste - - - - | awk '{c++; print "@illread"c"/2"; print $2; print "+"; print $4}' > data/illAll.2.fq
 fi
 
-if [ ! -r base.fa.amb ]
-then 
-  echo "Indexing genome for bwa"
-  bwa index base.fa
-fi
 
 if [ ! -r data/illAll.bam ]
 then
@@ -103,6 +109,9 @@ then
   freebayes -f base.fa data/illAll.bam -C $MIN_READS_FOR_SNP > data/illAll.vcf
 fi
 
+
+## Phase PacBio reads using Illumina SNPs
+###############################################################################
 
 if [ ! -r data/pbAll.hairs ]
 then
@@ -123,4 +132,52 @@ then
   java -jar ~/build/fgbio/target/scala-2.12/fgbio-0.2.1-SNAPSHOT.jar HapCutToVcf -i data/pbAll.hapcut -v data/illAll.vcf -o data/pbAll.phased.vcf
 fi
 
+if [ ! -r data/pbsnps/maternal.chain ]
+then
+  mkdir -p data/pbsnps
+  cd data/pbsnps
+  ln -s ../pbAll.phased.vcf
+  ln -s ../../base.fa
 
+  echo "constructing diploid sequence with SNPs"
+  java -jar /work-zfs/mschatz1/mschatz/build/vcf2diploid/vcf2diploid.jar -id unknown -chr base.fa -vcf pbAll.phased.vcf
+  cd ../..
+fi
+
+
+if [ ! -r data/spliced.vcf ]
+then
+  echo "Splicing in phased SVs"
+  ../../splicephase.pl data/pbAll.phased.vcf data/pbAll.sniffles.vcf data/pbAll.hairs data/spliced.vcf
+fi
+
+
+if [ ! -r data/spliceddiploid/maternal.chain ]
+then
+  mkdir -p data/spliceddiploid
+  cd data/spliceddiploid
+  ln -s ../spliced.vcf
+  ln -s ../../base.fa
+
+  echo "constructing diploid sequence with SNPs and SVs"
+  java -jar /work-zfs/mschatz1/mschatz/build/vcf2diploid/vcf2diploid.jar -id unknown -chr base.fa -vcf spliced.vcf
+  cd ../..
+fi
+
+if [ ! -r data/spliceddiploid/Am.delta ]
+then
+  echo "aligning diploid to truth"
+  nucmer -maxmatch data/mutA.fasta data/spliceddiploid/chr1_unknown_maternal.fa -p data/spliceddiploid/Am >& /dev/null
+  nucmer -maxmatch data/mutB.fasta data/spliceddiploid/chr1_unknown_maternal.fa -p data/spliceddiploid/Bm >& /dev/null
+  nucmer -maxmatch data/mutA.fasta data/spliceddiploid/chr1_unknown_paternal.fa -p data/spliceddiploid/Ap >& /dev/null
+  nucmer -maxmatch data/mutB.fasta data/spliceddiploid/chr1_unknown_paternal.fa -p data/spliceddiploid/Bp >& /dev/null
+fi
+
+if [ ! -r data/spliceddiploid/Am.1delta ]
+then
+  echo "filtering alignments"
+  delta-filter -1 data/spliceddiploid/Am.delta > data/spliceddiploid/Am.1delta
+  delta-filter -1 data/spliceddiploid/Bm.delta > data/spliceddiploid/Bm.1delta
+  delta-filter -1 data/spliceddiploid/Ap.delta > data/spliceddiploid/Ap.1delta
+  delta-filter -1 data/spliceddiploid/Bp.delta > data/spliceddiploid/Bp.1delta
+fi
